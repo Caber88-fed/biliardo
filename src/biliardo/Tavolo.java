07a30264dd2a8cb3b8dd12f09dd0648586885ea2
Caber88-fed <francescocaberlotto3@gmail.com>
2025-03-12 09:48:20 +0100

    ottimizzato inserimento palline

--- src/biliardo/Tavolo.java
+++ src/biliardo/Tavolo.java
@@ -58,59 +58,23 @@ public class Tavolo {
 				//palline
 				
 				//p[0]=new Pallina((3/4)*canvas.getBounds().width-Pallina.raggio, canvas.getBounds().height/2, 0, 0);
-				int x0=75*canvas.getBounds().width/100-Pallina.raggio;
-				int y0=canvas.getBounds().height/2;
-				int x1=x0+Pallina.raggio*2;
-				int y1=y0+Pallina.raggio;
-				int x2=x1;
-				int y2=y1-Pallina.raggio*2;
-				int x3=x1+Pallina.raggio*2;
-				int y3=y1+Pallina.raggio;
-				int x4=x3;
-				int y4=y3-Pallina.raggio*2;
-				int x5=x3;
-				int y5=y4-Pallina.raggio*2;
-				int x6=x3+Pallina.raggio*2;
-				int y6=y3+Pallina.raggio;
-				int x7=x6;
-				int y7=y6-Pallina.raggio*2;
-				int x8=x7;
-				int y8=y7-Pallina.raggio*2;
-				int x9=x8;
-				int y9=y8-Pallina.raggio*2;
+				int xb=75*canvas.getBounds().width/100-Pallina.raggio;
+				int yb=canvas.getBounds().height/2;
 				
-				
-				int x10=x6+Pallina.raggio*2;
-				int y10=y6+Pallina.raggio;
-				int x11=x10;
-				int y11=y10-Pallina.raggio*2;
-				int x12=x11;
-				int y12=y11-Pallina.raggio*2;
-				int x13=x12;
-				int y13=y12-Pallina.raggio*2;
-				int x14=x13;
-				int y14=y13-Pallina.raggio*2;
-				
+				for (int i = 0; i<5; i++) {
+					int yc = yb;
+					for (int ii = 0; ii<i+1; ii++) {
+						penna.fillOval(xb, yc, Pallina.raggio*2, Pallina.raggio*2);
+						yc-=Pallina.raggio*2;
+					}
+					xb += Pallina.raggio*2;
+					yb += Pallina.raggio;
+				}				
 				
 				int xx=25*canvas.getBounds().width/100-Pallina.raggio;
 				int yy=canvas.getBounds().height/2;
 				penna.fillOval(xx, yy, Pallina.raggio*2, Pallina.raggio*2);
 				
-				penna.fillOval(x0, y0, Pallina.raggio*2, Pallina.raggio*2);
-				penna.fillOval(x1, y1, Pallina.raggio*2, Pallina.raggio*2);
-				penna.fillOval(x2, y2, Pallina.raggio*2, Pallina.raggio*2);
-				penna.fillOval(x3, y3, Pallina.raggio*2, Pallina.raggio*2);
-				penna.fillOval(x4, y4, Pallina.raggio*2, Pallina.raggio*2);
-				penna.fillOval(x5, y5, Pallina.raggio*2, Pallina.raggio*2);
-				penna.fillOval(x6, y6, Pallina.raggio*2, Pallina.raggio*2);
-				penna.fillOval(x7, y7, Pallina.raggio*2, Pallina.raggio*2);
-				penna.fillOval(x8, y8, Pallina.raggio*2, Pallina.raggio*2);
-				penna.fillOval(x9, y9, Pallina.raggio*2, Pallina.raggio*2);
-				penna.fillOval(x10, y10, Pallina.raggio*2, Pallina.raggio*2);
-				penna.fillOval(x11, y11, Pallina.raggio*2, Pallina.raggio*2);
-				penna.fillOval(x12, y12, Pallina.raggio*2, Pallina.raggio*2);
-				penna.fillOval(x13, y13, Pallina.raggio*2, Pallina.raggio*2);
-				penna.fillOval(x14, y14, Pallina.raggio*2, Pallina.raggio*2);
 				display.sleep();
 			}
 		}
